e49f739a6e5d367205c426d118041029038868fc
aditi <aditi.abhang91@gmail.com>
2019-09-23 20:53:46 -0500

    practising numeric data formatting

---
+++ concepts_practice/numeric_data_formatting.py
@@ -0,0 +1,40 @@
+num = 3
+num1 = 3.69
+# returns the type of the numeric value
+print(type(num))
+print(type(num1))
+print("")
+
+# arithmatic operations
+print("Addition: ", 6 + 2)
+print("Substraction: ", 6 - 2)
+print("Multiplication: ", 6 * 2)
+print("Division: ", 6 / 2)
+print("Floor Division: ", 6 // 2)  # rounds off the result
+print("Exponent: ", 6 ** 2)
+print("Modulus: ", 6 % 2) # result is either 0 or 1
+print("")       
+
+# other built-in function
+print("Absolute value: ", abs(num))
+print("Round off: ", round(num1))
+print("Round off 1 digit after the decimal: ", round(num1, 1))
+print("")
+
+# comparisons
+print("num1 = 3")
+print("num2 = 3.69")
+print("Equals: ", num == num1)
+print("Not Equals: ", num != num1)
+print("Greater than: ", num > num1)
+print("Less than: ", num < num1)
+print("Greater than or equal: ", num >= num1)
+print("Less than or equals: ", num <= num1)
+print("")
+
+# str to int 
+num2 = (int('100'))
+num3 = (int('200'))
+print("Str to int: ",num2)
+print("Adding two str-to-int: ",num2 + num3)
+
